8071e3b50ff348c95d4445ef1f02cba964d5e4b9
Mehmet Halis <mehmethaliscicek@gmail.com>
2022-09-26 23:37:15 +0300

    add github records

--- app/candidate/views.py
+++ app/candidate/views.py
@@ -1,6 +1,9 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from core.models import Candidate
 from candidate import serializers
+import requests
+
+# from drf_spectacular.utils import extend_schema_view, extend_schema
 
 
 class CandidateViewSet(viewsets.ModelViewSet):
@@ -8,11 +11,58 @@ class CandidateViewSet(viewsets.ModelViewSet):
     queryset = Candidate.objects.all()
 
     def get_queryset(self):
-        # payload = {'key1': 'value1', 'key2': ['value2', 'value3']}
+        queryset = self.queryset
 
-        # r = requests.get("https://jsonplaceholder.typicode.com/posts")
-        # print(r.text)
-        return super().get_queryset().filter().order_by("-id")
+        items = queryset.filter().order_by("id").distinct()
+        # save db from github
+        if len(items) == 0:
+            try:
+                r = requests.get(
+                    "https://api.github.com/search/users?q=location:Turkey"
+                )
+                data = r.json()
+
+                for user in data["items"]:
+                    url = "https://api.github.com/users/" + user["login"]
+                    res = requests.get(url)
+
+                    user_data = res.json()
+                    name = user_data["name"]
+                    website = user_data["blog"]
+                    location = user_data["location"]
+                    email = user_data["email"]
+                    bio = user_data["bio"]
+                    join_date = user_data["created_at"]
+                    hireable = user_data["hireable"]
+                    company = user_data["company"]
+
+                    b = {
+                        "name": name,
+                        "username": user["login"],
+                        "avatar_url": user["avatar_url"],
+                        "github_url": user["html_url"],
+                        "website": website,
+                        "location": location,
+                        "email": email,
+                        "bio": bio,
+                        "join_date": join_date,
+                        "company": company,
+                        "hireable": hireable,
+                    }
+
+                    serializer = serializers.CandidateSerializer(data=b)
+                    serializer.is_valid(raise_exception=True)
+                    self.perform_create(serializer)
+                items = queryset.filter().order_by("id").distinct()
+            except Exception as e:
+                print(e)
+
+        role = self.request.query_params.get("role")
+
+        if role:
+            items = queryset.filter(bio__icontains=role)
+
+        return items
 
     def get_serializer_class(self):
         if self.action == "list":
@@ -22,3 +72,19 @@ class CandidateViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save()
+
+
+class BaseCandidateViewSet(
+    viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin
+):
+    def get_queryset(self):
+        queryset = self.queryset
+        return queryset.filter().order_by("-name").distinct()
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+
+class Candidate(BaseCandidateViewSet):
+    queryset = Candidate.objects.all()
+    serializer_class = serializers.CandidateSerializer
--- app/core/models.py
+++ app/core/models.py
@@ -4,15 +4,15 @@ from django.db import models
 class Candidate(models.Model):
     name = models.CharField(max_length=100)
     username = models.CharField(max_length=100, unique=True)
-    avatar_url = models.CharField(max_length=100, blank=True)
+    avatar_url = models.CharField(max_length=100, blank=True, null=True)
     github_url = models.CharField(max_length=100)
-    website = models.CharField(max_length=100, blank=True)
+    website = models.CharField(max_length=100, blank=True, null=True)
     location = models.CharField(max_length=100)
-    email = models.EmailField(max_length=255, blank=True)
-    bio = models.CharField(max_length=100)
+    email = models.EmailField(max_length=255, blank=True, null=True)
+    bio = models.CharField(max_length=250, blank=True, null=True)
     join_date = models.CharField(max_length=100)
-    hireable = models.BooleanField(blank=True)
-    company = models.CharField(max_length=100, blank=True)
+    hireable = models.BooleanField(blank=True, null=True)
+    company = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.username
